a9e0bafcfb2038c6682cbf74cae46d553a02414b
Dmitry Mina <snigavig@gmail.com>
2014-02-09 06:21:17 +0200

    Google Earth API cleanup + additional functionality

--- OutOfSpace/OutOfSpace.Web_HotTowel/app/skymap/googleEarth.js
+++ OutOfSpace/OutOfSpace.Web_HotTowel/app/skymap/googleEarth.js
@@ -1,13 +1,9 @@
 ﻿function initGoogleEarth () {
 //    'use strict';
-    console.log('im in');
     var ge;
-    //var $q = common.$q;
-    //google.load("earth", "1", { "other_params": "sensor=false", "callback": function () { console.log('callback fired') } })
-    //;
+
     function init() {
         //q.when()
-        console.log('init');
         return google.earth.createInstance('skymap', initCB, failureCB);
     }
     function failureCB(errorCode) {
@@ -28,15 +24,51 @@
 
         ge.getWindow().setVisibility(true);
     }
-    //google.load("earth", "1", { "other_params": "sensor=false" }).done(function () {
-    //    google.setOnLoadCallback(init);
-    //});
+    function flyToObject(ge, spaceObject) {
 
+        var oldFlyToSpeed = ge.getOptions().getFlyToSpeed();
+        ge.getOptions().setFlyToSpeed(.32); // Slow down the camera flyTo speed.
+        var lookAt = ge.createLookAt('');
 
-    //google.setOnLoadCallback(
+        lookAt.set(spaceObject.lat, spaceObject.lng, spaceObject.alt,
+        spaceObject.alt_enum, spaceObject.heading,
+        spaceObject.tilt, spaceObject.range);
+
+        ge.getView().setAbstractView(lookAt);
+        ge.getOptions().setFlyToSpeed(oldFlyToSpeed);
+    }
+
+    function definePlacemark(ge, spaceObject) {
+        var placemark = ge.createPlacemark('');
+
+        // Define a custom icon.
+        var icon = ge.createIcon('');
+        icon.setHref('http://s5.hostingkartinok.com/uploads/images/2014/02/7d83bacb06f30d0f7d90956ea178fbc4.png');//Death Star image for now
+        var style = ge.createStyle(''); //create a new style
+        style.getIconStyle().setIcon(icon); //apply the icon to the style
+        style.getIconStyle().setScale(3.0); // apply icon size
+        placemark.setStyleSelector(style); //apply the style to the placemark
+
+        // Set the placemark's location.
+        var point = ge.createPoint('');
+        point.setLatitude(spaceObject.lat);
+        point.setLongitude(spaceObject.lng);
+        placemark.setGeometry(point);
+        // give the placemark a name and a description (a balloon will
+        // automatically show on click)
+        placemark.setName(spaceObject.name);
+        placemark.setDescription(spaceObject.description);
+
+        google.earth.addEventListener(placemark, 'click', function (event) {
+            // Prevent the default balloon from popping up.
+            event.preventDefault();
+            //TODO: implement ClickEventListener
+        });
+
+        // add the placemark to the earth DOM
+        ge.getFeatures().appendChild(placemark);
+    }
+
         init();
-        //);
-    //setTimeout(init, 2000);
 
 }
-//)();
