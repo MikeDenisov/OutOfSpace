d84d2baffd1d1bc6ee73f3b7994a41644cba9049
MikeDenisov <denisov.ua@gmail.com>
2014-02-09 14:13:33 +0200

    Added REST API for Carma

---
+++ OutOfSpace/OutOfSpace.Web_HotTowel/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OutOfSpace.Web.Models
+{
+    public class User
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Carma Carma { get; set; }
+    }
+}
