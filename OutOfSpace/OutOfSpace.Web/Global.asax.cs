618be816e5df542e1f713252efb1878ba0996f65
MikeDenisov <denisov.ua@gmail.com>
2014-02-23 16:26:41 +0200

    Added: Authorization API; OWIN libs and Startup configuration; API help page.

--- OutOfSpace/OutOfSpace.Web/Global.asax.cs
+++ OutOfSpace/OutOfSpace.Web/Global.asax.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
+using System.Web.Mvc;
 using OutOfSpace.Web.App_Start;
 
 namespace OutOfSpace.Web
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
---
+++ OutOfSpace/OutOfSpace.Web/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(OutOfSpace.Web.Startup))]
+namespace OutOfSpace.Web
+{
+
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+
+        }
+    }
+}
